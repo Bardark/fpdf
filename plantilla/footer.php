325972b17401a77a2a4de13716d2d2a455bf0cce
Bardark <ren_rbk@hotmail.es>
2017-10-20 10:02:14 -0500

    descarga de js faltantes

--- plantilla/footer.php
+++ plantilla/footer.php
@@ -6,10 +6,13 @@
 <script src="js/bootstrap.min.js"></script>
 <!-- JS para editar o eliminar registro -->
 <script src="js/agregar.js"></script>
+<!-- Funciones propias y el Datepicker -->
+<script src="js/funciones.js"></script>
+<!-- SWEET alert -->
+<script src="js/sweetalert.min.js"></script>
 
 <!-- Datepicker -->
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js'></script>
-<script src='http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.min.js'></script>
-<script src='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/3.1.3/js/bootstrap-datetimepicker.min.js'></script>
-<script src="js/funciones.js"></script>
+<script src='js/jquery.min.js'></script>
+<script src='js/bootstrap3.3.2.min.js'></script>
+<script src='js/moment-with-locales.min.js'></script>
+<script src='js/bootstrap-datetimepicker.min.js'></script>
